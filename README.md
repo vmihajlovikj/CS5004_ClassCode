ee38ef81ac657a84812750573be50f8fbb4efc03
Vidoje Mihajlovikj <vidojemihajlovikj@Vidojes-MacBook-Pro.local>
2026-05-08 10:28:46 -0400

    ClassCode Fixed

---
+++ README.md
@@ -0,0 +1,1 @@
+Please refer to the Canvas description for this assignment.
---
+++ src/test/java/VectorTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * The test class for Vector3D.
+ */
+public class VectorTest {
+  @Test
+  public void testConstructor1() {
+    assertTrue(true);
+  }
+}
